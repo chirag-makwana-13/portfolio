087e4dc46b229a55930b1338edbbdbfb33d0a7ac
chirag-makwana-13 <chirag.makwana.2024@gmail.com>
2025-02-19 17:25:37 +0530

    Url changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,11 +2,8 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 
 function Projects() {
@@ -68,8 +65,8 @@ function Projects() {
               isBlog={false}
               title="Personal Portfolio ( React )"
               description="A modern and responsive portfolio website built with React, showcasing projects, skills, experience, and contact details. It features smooth animations, dark/light mode, and dynamic content for an interactive user experience."
-              // ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              ghLink="https://github.com/chirag-makwana-13/portfolio"
+              demoLink="https://chirag-makwana-13.github.io/portfolio"
             />
           </Col>
 
